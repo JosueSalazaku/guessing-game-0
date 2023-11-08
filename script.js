4a620cdfade13aecca7557c534cbaa65894bc070
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-08 11:57:12 +0100

    Create a function to compare playerInput and randomNumber

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 const playerInput = document.querySelector("#numbers");
 const playBtn = document.querySelector(".play");
+
+function play() {
+  if (randomNumber === playerInput) {
+    const guess = parseInt(playerInput.value);
+
+    alert(
+      "Awesome! You number (actual number) was correct. You can be named many things, hungry not being one of them."
+    );
+  } else {
+    alert(
+      "Bummer... You guessed (guess) and the secret number was " + randomNumber
+    );
+  }
+}
+console.log(randomNumber);
+console.log(playerInput.value);
